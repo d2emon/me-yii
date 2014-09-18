033b51136de177f8c575746b96822d7f9647abda
Dmitry Kutsenko <d2emonium@gmail.com>
2014-09-18 03:18:17 +0300

    Using PHPStorm

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,19 +47,19 @@ return array(
 			),
 		),
 		*/
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		*/
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=me',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'me',
+			'password' => 'e3sM6nJP4bH8pGcQ',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
